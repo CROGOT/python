11e54c362b069cc366220fc6664143b17cfd5f81
CROGOT <48329827+CROGOT@users.noreply.github.com>
2019-03-13 18:27:44 +0300

    6

--- kinopoisk.py
+++ kinopoisk.py
@@ -2,26 +2,49 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import os
+import configparser
 
-SITE_URL="https://www.kinopoisk.ru/film/706655/"
-PICS_URL=SITE_URL+"stills/"
+#######################################################
+
+SITE_URL="https://www.kinopoisk.ru/film/994634/"
+
 CATALOG="C:\\Users\\Егор\\Desktop\\foto\\"
-FILE_NAME="Капитан_Марвел.html"
-STEP=1 # 1-спарсить сайт в файл, 2 - спарсить инфу по фильму, 3 - сохранить картинки в каталог
 
-film={}
+STEP=[1,2,3,4] 
+	   # 1 - спарсить сайт в файлы, 
+	   # 2 - спарсить инфу по фильму, 
+	   # 3 - парс урл картинок,
+	   # 4 - сохранить картинки в каталог
 
-def save_htm_into_file(url='',name_catalog='',filename=''):
-	if STEP != 1: return false
-	page = requests.get(url,'windows-1251')
-	soup = BeautifulSoup(page.text,'lxml')
-	prefix=re.sub(':', '',soup.title.text)
-	if name_catalog=='': name_catalog=os.getcwd()+"\\"+prefix
-	else: name_catalog+="\\"+prefix
-	if filename=='': filename=prefix+'.html'
-	if (filename=='') and (name_catalog==PICS_URL): filename=prefix+'_pics.html'
-	# print(name_catalog)
+#######################################################
 
+config = configparser.ConfigParser()
+inf = configparser.ConfigParser()
+INFO='INFO'
+SETTING='SETTING'
+PICS='PICS'
+FILEINI='example.ini'
+INFOINI='info.ini'
+PICS_URL=SITE_URL+"stills/"
+# FILE_NAME="КиноПоиск.html"
+film = {}
+
+def init():
+		config.read(FILEINI)
+		if config.sections()==[]:
+			print ("Прочитать настройки из %s не удалось" % FILEINI)
+			config[SETTING]={}
+			config[PICS]={}
+			config[INFO]={}
+			config[SETTING]['site_url']=SITE_URL
+			config[SETTING]['pics_url']=PICS_URL
+			config[SETTING]['step']=str(STEP)
+		else: 
+			print ("Настройки из %s прочитаны" % FILEINI)
+			path=config[SETTING]['name_catalog']+'\\'+INFOINI
+			inf.read(path)
+
+def save_htm_into_file(page,name_catalog='',filename=''):
 	try:
 		os.mkdir(name_catalog)
 	except OSError: 
@@ -30,7 +53,6 @@ def save_htm_into_file(url='',name_catalog='',filename=''):
 		print ("Успешно создана директория %s " % name_catalog)
 	
 	path=name_catalog+'\\'+filename
-
 	try:
 		f=open(path,'w')
 	except OSError:
@@ -40,80 +62,138 @@ def save_htm_into_file(url='',name_catalog='',filename=''):
 	f.write(page.text)
 	f.close()
 
-def kinopoisk_save_pic(url,destination):
-	if STEP != 3: return false
-	# page = requests.get(url,'utf-8')
-	f=open('kinopoisk.html')
-	text = f.read()
-	f.close()
-	soup = BeautifulSoup(text,'lxml')
+def save_main_html():
+	if (1 not in STEP): return False
+	page = requests.get(SITE_URL,'windows-1251')
+	soup = BeautifulSoup(page.text,'lxml')
+	prefix=re.sub(':', '',soup.title.text)
+	name_catalog = CATALOG+prefix
+	filename=prefix+'.html'
+	config[SETTING]['name_catalog']=name_catalog
+	config[SETTING]['filename']=filename
+	write_ini()
+	save_htm_into_file(page,name_catalog,filename)
 
-	table_pic = soup.find('table',{'class':'js-rum-hero fotos'}).findAll('img')
-	for img in table_pic:
-		url_pic=re.sub('sm_', '', img.get('src'))
-		img_name = url_pic.split('/')[-1]
-		print(img_name)
-
-def kinopoisk_save_info_film(url,destination):
-	if STEP != 2: return false
+def save_pics_html():
+	if (1 not in STEP): return False
+	page = requests.get(PICS_URL,'windows-1251')
+	soup = BeautifulSoup(page.text,'lxml')
+	prefix=re.sub(':', '',soup.title.text)
+	filename=prefix+'.html'
+	name_catalog = config[SETTING]['name_catalog']
+	config[SETTING]['filenamepis']=filename
+	save_htm_into_file(page,name_catalog,filename)
+	
+def kinopoisk_save_info_film():
+	if (2 not in STEP): return False
 	# page = requests.get(url)
 	# page.encoding = 'utf-8'
-	f=open('kinopoisk_main.html')
+	path=config[SETTING]['name_catalog']+'\\'+config[SETTING]['filename']
+	f=open(path)
 	text = f.read()
 	f.close()
 	soup = BeautifulSoup(text,'lxml')
 	film['имя']=soup.find('h1',{'class':'moviename-big'}).text
 	table_info = soup.find('table',class_='info')
-	# print(table_info)
-	# print(table_info[0].contents[1].text)
-	# god=re.sub('\n', '',table_info[0].contents[3].text)
+
 	film['год']=table_info.find('td',text = re.compile('год')).findNext('td').a.text
-	# print("Год: "+film['год'])
 
-	# print(table_info[1].contents[1].text)
-	# strana=re.sub('\n', '',table_info[1].contents[3].text)
-	film['страна']=table_info.find('td',text = re.compile('страна')).findNext('td').a.text
-	# print("Страна: "+film['страна'])
+	film['страна']=[]
+	for strana in table_info.find('td',text = re.compile('страна')).findNext('td').findAll('a'):
+		film['страна'].append(strana.text)
+	# film['страна']=table_info.find('td',text = re.compile('страна')).findNext('td').a.text
 
-	# print(table_info[2].contents[0].text)
-	# slogan=re.sub('\n', '',table_info[2].contents[1].text)
 	film['слоган']=table_info.find('td',text = re.compile('слоган')).findNext('td').text
-	# print("Слоган: "+film['слоган'])
 
-	# print(table_info[3].contents)
 	film['режиссер']=[]
 	for regiser in table_info.find('td',text = re.compile('режиссер')).findNext('td').findAll('a'):
 		film['режиссер'].append(regiser.text)
-	# film['режиссер']=film['режиссер'][:-1]
-	# print("Режиссёр:" + film['режиссер'])
 
+	try:
+		film['бюджет']=table_info.find('td',text = re.compile('бюджет')).findNext('td').a.text
+	except AttributeError:
+		try:
+			film['бюджет']=table_info.find('td',text = re.compile('бюджет')).findNext('td').div.text.replace("\n", "").replace(" ", "")
+		except AttributeError:
+			film['бюджет']='-'
+		
+	film['время']=table_info.find('td',text = re.compile('время')).findNext('td').text
+
+	try:
+		film['дата']=soup.find('td',{'id':'div_rus_prem_td2'}).find('div',{'class':'prem_ical'}).get('data-ical-date')
+	except AttributeError:
+		film['дата']='-'
+	
 	film['возраст']=re.sub('"', '',str(table_info.find('td',text = re.compile('возраст')).findNext('td').contents[1])[24:26])
-	# print("Возраст: "+film['возраст']+" +")
 
-
-	aktery = soup.findAll('li',{'itemprop':'actors'})
+	aktery = soup.find('div',{'id':'actorList'}).ul
 	film['актер']=[]
-	for akter in aktery:
+	for akter in aktery.findAll('li'):
 		film['актер'].append(akter.contents[0].text)
-	# print("Актёры: "+film['актер'])
+	film['актер'].pop(-1)
+
 	film['preview_url']=soup.find('a',{'class':'popupBigImage'}).img.get('src')
 
-	print(film)
-	print(soup.title.text)
-	path = os.getcwd()
-	print ("Текущая рабочая директория %s" % path)
-	# print(soup.find('a',{'class':'popupBigImage'}).img.get('src'))
-	# td=soup.find('td',text = re.compile('год')).next.next.next.a.text
-	# print(table_info[3].contents[1].findAll('a'))
-	# print(table_info[3].contents[1].text)[24:26][24:26]
-	# print(table_info[3].contents[3].text)		
-	# for info in table_info:
+	config[INFO]={}
+	for item in film:
+		if type(film[item])==list:
+			config[INFO][item]=','.join(film[item])
+		else:
+			config[INFO][item]=str(film[item])
+	for key in config['INFO']:
+		print(key+': '+config['INFO'][key])
+	inf[INFO]=config[INFO]
+	# path = os.getcwd()
+	# print ("Текущая рабочая директория %s" % path)
 	# print(info.contents[0])
-	
 
+def write_ini():
+	with open(FILEINI, 'w') as configfile:
+		config.write(configfile)
+def write_info_ini():
+	path=config[SETTING]['name_catalog']+'\\'+INFOINI
+	with open(path, 'w') as infofile:
+		inf.write(infofile)
+
+def get_ini(key):
+	config.read(FILEINI)
+	return config[SETTING][key]
+
+def kinopoisk_save_urlpic():
+	if (3 not in STEP): return False
+	# page = requests.get(url,'utf-8')
+	path=config[SETTING]['name_catalog']+'\\'+config[SETTING]['filenamepis']
+	f=open(path)
+	text = f.read()
+	f.close()
+	soup = BeautifulSoup(text,'lxml')
+	table_pic = soup.find('table',{'class':'js-rum-hero fotos'}).findAll('img')
+	config[PICS]={}
+	config[PICS]['preview_url'+'.jpg']=config['INFO']['preview_url']
+	for img in table_pic:
+		url_pic=re.sub('sm_', '', img.get('src'))
+		img_name = url_pic.split('/')[-1]
+		config[PICS][img_name]=url_pic
+		print(img_name)
+	inf[PICS]=config[PICS]
+
+def kinopoisk_save_pics():
+	if (4 not in STEP): return False
+	for key in config[PICS]:
+		path=config[SETTING]['name_catalog']+'\\'+key
+		# print(key,config[PICS][key] )
+		p = requests.get(config[PICS][key])
+		out = open(path, "wb")
+		out.write(p.content)
+		out.close()
+		print(path)
 
 if __name__ == "__main__":
-	save_htm_into_file(SITE_URL,CATALOG)
-	save_htm_into_file(PICS_URL,CATALOG)	
-	# kinopoisk_save_pic('https://www.kinopoisk.ru/film/843859/stills/',CATALOG)
-	# kinopoisk_save_info_film(SITE_URL,'')
+	init()
+	save_main_html()
+	save_pics_html()
+	kinopoisk_save_info_film()
+	kinopoisk_save_urlpic()
+	kinopoisk_save_pics()
+	write_ini()
+	write_info_ini()
